5d4c9c8a12c7411c9d5bb60fa2dbe0d22ca5b18f
DanielgDiaz <danielgdiaz92@gmail.com>
2022-03-24 21:24:23 +0100

    static method showMenu

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
+import static ui.UIMenu.*;
+
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor("Dani");
 
-
+        Doctor myDoctor = new Doctor();
         myDoctor.name = "Batman";
         myDoctor.showName();
+        myDoctor.showId();
+
+        Doctor MyDoctorAnn = new Doctor();
+        myDoctor.showId();
+
+        showMenu();
     }
+
+
 }
