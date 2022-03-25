d49c5aac67afc547ed376f6b787ec00b951f941d
DanielgDiaz <danielgdiaz92@gmail.com>
2022-03-25 20:34:30 +0100

    for i final MONTHS

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,6 @@ import static ui.UIMenu.*;
 public class Main {
     public static void main(String[] args) {
 
-        Doctor myDoctor = new Doctor();
-        myDoctor.name = "Batman";
-        myDoctor.showName();
-        myDoctor.showId();
-
-        Doctor MyDoctorAnn = new Doctor();
-        myDoctor.showId();
-
         showMenu();
     }
 
