de4cda402fb81604c463c1b5c24c259fe1b599a8
DanielgDiaz <danielgdiaz92@gmail.com>
2022-03-28 18:57:06 +0200

    getter & setters patient class

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,20 @@ import static ui.UIMenu.*;
 public class Main {
     public static void main(String[] args) {
 
-        showMenu();
+        //showMenu();
 
         Doctor myDoctor = new Doctor("Dani", "Surgeon" );
         System.out.println(myDoctor.name);
         System.out.println(myDoctor.speciality);
+
+        Patient patient = new Patient("Rigoberto", "rigo@email.com");
+        patient.setWeight(80.5);
+        System.out.println(patient.getWeight());
+
+        patient.setPhoneNumber("664109778");
+        System.out.println(patient.getPhoneNumber());
+
+
     }
 
 
---
+++ src/Patient.java
@@ -0,0 +1,93 @@
+public class Patient {
+    int id;
+    private String name;
+    private String email;
+    private String address;
+    private String phoneNumber;
+    private String birthday;
+    private double weight;
+    private double height;
+    private String blood;
+
+
+    Patient (String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public void setWeight (double weight) {
+        this.weight = weight;
+    }
+
+    public String getWeight(){
+        return this.weight + " Kg.";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        if (phoneNumber.length() != 9){
+            System.out.println("Invalid phone number");
+        }
+        else if (phoneNumber.length() == 9) {
+                this.phoneNumber = phoneNumber;}
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getHeight() {
+        return height + " M.";
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public String getBlood() {
+        return blood;
+    }
+
+    public void setBlood(String blood) {
+        this.blood = blood;
+    }
+}
